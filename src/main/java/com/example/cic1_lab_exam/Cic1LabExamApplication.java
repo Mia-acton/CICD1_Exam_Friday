ad3b06012ea217212d126792ae34f26184d3428a
MIAACTON-STUDENT <G00428601@atu.ie>
2025-11-21 09:06:25 +0000

    Created new project

---
+++ src/main/java/com/example/cic1_lab_exam/Cic1LabExamApplication.java
@@ -0,0 +1,13 @@
+package com.example.cic1_lab_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cic1LabExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cic1LabExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cic1_lab_exam/Cic1LabExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cic1_lab_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cic1LabExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
